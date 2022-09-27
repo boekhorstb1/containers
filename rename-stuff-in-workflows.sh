17eefb428e24bff4cb93657220cd182c335473d5
Rafael te Boekhorst <boekhorst@b1-systems.de>
2022-09-27 12:03:32 +0200

    renamed workflows to boekhorstb1

--- rename-stuff-in-workflows.sh
+++ rename-stuff-in-workflows.sh
@@ -7,5 +7,5 @@ for f in $UPDATEDIR
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  sed -i 's+ghcr.io/boekhorstb1/containers+ghcr.io/maintaina/containers+g' $f
+  sed -i 's+ghcr.io/maintaina/containers+ghcr.io/boekhorstb1/containers+g' $f
 done
